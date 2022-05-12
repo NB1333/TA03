649f9b51dab71c4c5e48d859644c113eb2579fa0
NB1333 <bykov.nikita@lll.kpi.ua>
2022-05-12 20:09:28 +0300

    particulary ready

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from datetime import datetime
 import time
+from random import *
+
 
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
+
+    def find(self, item):
+
+        while self.head is not None:
+            if self.head.data == item:
+                return True
+            self.head = self.head.next
+        return False
 
     def beginAdd(self, new_data):
         new_node = Node(new_data)
@@ -37,8 +48,51 @@ class LinkedList:
         new_node.next = previous_node.next
         previous_node.next = new_node
 
-    #def deleteBeginning(self):
-        
+    def deletion(self, data):
+        if self.head is None:
+            return
+
+        temp = self.head
+
+        if data == 0:
+            self.head = temp.next
+            temp = None
+            return
+
+        for i in range(data - 1):
+            temp = temp.next
+            if temp is None:
+                break
+
+        if temp is None:
+            return
+
+        if temp.next is None:
+            return
+
+        next = temp.next.next
+        temp.next = None
+        temp.next = next
+
+    def deleteFirstElement(self):
+        if self.head != None:
+            temp = self.head
+
+            self.head = self.head.next
+
+            temp = None
+
+    def deleteLastElement(self, data):
+        if data is None:
+            return None
+        if data.next is None:
+            data = None
+            return None
+        second_last = data
+        while (second_last.next.next):
+            second_last = second_last.next
+        second_last.next = None
+        return data
 
     def print(self):
         temp = self.head
@@ -46,17 +100,57 @@ class LinkedList:
             print(temp.data, end=" ")
             temp = temp.next
 
+    def clear(self):
+        while (self.head != None):
+            temp = self.head
+            self.head = self.head.next
+            temp = None
+
 
 if __name__ == '__main__':
+    newString = '\n'
+    Range = 1000
     start_time = datetime.now()
+
     llist = LinkedList()
-    llist.beginAdd(4)
-    llist.endAdd(34)
-    llist.endAdd(6)
-    llist.endAdd(3)
-    llist.insertAfterIndex(2, 5)
+
+    start_time1 = datetime.now()
+    for i in range(Range):
+        llist.beginAdd(randint(10, 8000))
+    print('Insert time at the begin: ' + str(datetime.now() - start_time1) + newString)
+
+    llist.clear()
+
+    start_time2 = datetime.now()
+    for i in range(Range):
+        llist.endAdd(randint(10, 8000))
+    print('Insert time at the end: ' + str(datetime.now() - start_time2) + newString)
+
+    llist.clear()
+    llist.beginAdd(12)
+    llist.beginAdd(2)
+
+    start_time3 = datetime.now()
+    for i in range(Range):
+        llist.insertAfterIndex(llist.head.next, randint(10, 8000))
+    print('Insert time after index: ' + str(datetime.now() - start_time3) + newString)
+
     llist.print()
 
-    time.sleep(5)
-    print(datetime.now() - start_time)
+    start_time4 = datetime.now()
+    llist.deleteFirstElement()
+    print('\n\nDeletion time of the first element: ' + str(datetime.now() - start_time4) + newString)
+    llist.print()
+    head = llist.endAdd(1337)
 
+    start_time5 = datetime.now()
+    llist.deleteLastElement(head)
+    print('\nDeletion time of the last element: ' + str(datetime.now() - start_time5) + newString)
+    llist.print()
+    start_time6 = datetime.now()
+    llist.deletion(3)
+    print('\nDeletion time after index: ' + str(datetime.now() - start_time6) + newString)
+    llist.print()
+    start_time7 = datetime.now()
+    llist.find(1337)
+    print('Finding time: ' + str(datetime.now() - start_time7) + newString)
